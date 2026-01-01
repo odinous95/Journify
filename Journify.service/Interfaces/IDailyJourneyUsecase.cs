8b3a59530edc8940ec56e50dab72824fa9568b22
odinous95 <mjd.thif@icloud.com>
2026-01-01 23:19:45 +0100

    Add use case and repository for DailyJourney entities
    Introduced IDailyJourneyRepository and IDailyJourneyUsecase interfaces to define repository and use case operations for DailyJourney management. Implemented DailyJourneyUsecase to handle business logic and delegate data access to the repository, improving separation of concerns.

---
+++ Journify.service/Interfaces/IDailyJourneyUsecase.cs
@@ -0,0 +1,11 @@
+﻿using Journify.core.Entities;
+
+namespace Journify.service.Interfaces
+{
+    public interface IDailyJourneyUsecase
+    {
+        Task<DailyJourney> CreateJourneyAsync(DailyJourney journey);
+        Task<IEnumerable<DailyJourney>> GetAllJourneysAsync();
+        Task<DailyJourney> GetJourneyByIdAsync(Guid id);
+    }
+}
---
+++ Journify.service/usecases/DailyJourneyUsecase.cs
@@ -0,0 +1,26 @@
+﻿using Journify.core.Entities;
+using Journify.service.Interfaces;
+
+namespace Journify.service.usecases
+{
+    public class DailyJourneyUsecase : IDailyJourneyUsecase
+    {
+        private readonly IDailyJourneyRepository _journeyRepository;
+        public DailyJourneyUsecase(IDailyJourneyRepository journeyRepository)
+        {
+            _journeyRepository = journeyRepository;
+        }
+        public async Task<DailyJourney> CreateJourneyAsync(DailyJourney journey)
+        {
+            return await _journeyRepository.AddJourneyAsync(journey);
+        }
+        public async Task<IEnumerable<DailyJourney>> GetAllJourneysAsync()
+        {
+            return await _journeyRepository.GetAllJourneysAsync();
+        }
+        public async Task<DailyJourney> GetJourneyByIdAsync(Guid id)
+        {
+            return await _journeyRepository.GetJourneyById(id);
+        }
+    }
+}
