04d4573fd697a214173aaa47ab6ab21450cc0855
odinous95 <mjd.thif@icloud.com>
2026-01-02 13:13:17 +0100

    Add IUserRepository and IUserService interfaces
    Introduce IUserRepository and IUserService in Journify.service.Interfaces, defining async methods for user creation and retrieval using the User entity.

---
+++ Journify.service/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Journify.core.Entities;
+
+namespace Journify.service.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> AddUserAsync(User user);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserById(Guid id);
+
+    }
+}
---
+++ Journify.service/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using Journify.core.Entities;
+
+namespace Journify.service.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User> AddUserAsync(User user);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> GetUserById(Guid id);
+    }
+}
