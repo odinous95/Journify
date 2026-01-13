f71cb149a37a177520ee025d57363c3d66551a4e
odinous95 <mjd.thif@icloud.com>
2026-01-13 23:10:36 +0100

    Replace Weather API with UserController, add DI & Swagger
    Removed WeatherForecast sample API. Added UserController with endpoints for user CRUD operations using IUserService. Refactored Program.cs to support dependency injection, PostgreSQL via environment variables, global exception handling, and Swagger UI. Updated project dependencies for DotNetEnv, EF Core Design, and Swashbuckle.

---
+++ UserManagmentService/src/UserManagment.api/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserManagment.service.Interfaces;
+
+namespace UserManagment.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult> GetUserByIdAsync(Guid id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null) return NotFound("User not found.");
+            return Ok(user);
+
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser([FromBody] Journify.core.Entities.User user)
+        {
+            if (user == null) return BadRequest("User data is null.");
+            var createdUser = await _userService.AddUserAsync(user);
+            return Ok(createdUser);
+
+        }
+        [HttpGet]
+        public async Task<ActionResult> GetAllUsersAsync()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            if (users == null) return NotFound("No users found.");
+            return Ok(users);
+        }
+    }
+}
--- UserManagmentService/src/UserManagment.api/Program.cs
+++ UserManagmentService/src/UserManagment.api/Program.cs
@@ -1,17 +1,40 @@
+using DotNetEnv;
+using Microsoft.EntityFrameworkCore;
+using UserManagment.api.Filters;
+using UserManagment.infrastructure.Data;
+using UserManagment.Infrastructure.Repository;
+using UserManagment.service.Interfaces;
+using UserManagment.service.usecases;
+
+
+Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(
+    options => options.Filters.Add<GloblaExceptionFilter>()
+);
+// Register Repositories and Services for Steps
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserService, UserServices>();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Add Dev Database                   
+var connectionString = Environment.GetEnvironmentVariable("PostgreSqlConnection");
+// Add DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+// Register Swagger generator (required for ISwaggerProvider)
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
 }
 
 app.UseAuthorization();
