32c31f9e903e4a27eba6427b1fa554ffea6b4eab
odinous95 <mjd.thif@icloud.com>
2026-01-17 13:32:28 +0100

    Refactor DailyJourney properties and add JounreyName
    Removed User and JournyDate properties from DailyJourney class. Added JounreyName string property initialized to an empty value.

--- Journify.core/Entities/DailyJourney.cs
+++ Journify.core/Entities/DailyJourney.cs
@@ -4,9 +4,8 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public Guid UserId { get; set; }
-        public User User { get; set; } = null!;
+        public string JounreyName { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateOnly JournyDate { get; set; }
         public ICollection<Step> Entries { get; set; } = new List<Step>();
     }
 }
