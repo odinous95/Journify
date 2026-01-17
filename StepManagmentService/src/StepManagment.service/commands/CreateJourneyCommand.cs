6456f56677c74f6f1ed8c6497627c834ff75ff4e
odinous95 <mjd.thif@icloud.com>
2026-01-17 13:34:24 +0100

    Add CreateJourneyCommand class for journey creation
    Added CreateJourneyCommand.cs in StepManagment.service.commands namespace. This class encapsulates user ID and journey name properties, providing a constructor for initialization to support journey creation functionality.

---
+++ StepManagmentService/src/StepManagment.service/commands/CreateJourneyCommand.cs
@@ -0,0 +1,14 @@
+﻿
+namespace StepManagment.service.commands
+{
+    public class CreateJourneyCommand
+    {
+        public Guid UserId { get; }
+        public string JourneyName { get; }
+        public CreateJourneyCommand(Guid userId, string journeyName)
+        {
+            UserId = userId;
+            JourneyName = journeyName;
+        }
+    }
+}
