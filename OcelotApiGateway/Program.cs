641879b2499deda73a07fe27294a80badd4bbfe8
odinous95 <mjd.thif@icloud.com>
2026-01-17 13:37:32 +0100

    Update middleware import and enable console logging
    Switched from OcelotApiGateway.Middleware to Ocelot.Middleware in Program.cs for improved compatibility. Added builder.Logging.AddConsole() to enable console logging for better diagnostics.

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -5,6 +5,9 @@ using OcelotApiGateway.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+
+builder.Logging.AddConsole();
+
 var app = builder.Build();
 
 
