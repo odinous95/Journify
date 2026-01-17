e2b2e8245920d92744651dcd79d96d7fce97ea94
odinous95 <mjd.thif@icloud.com>
2026-01-17 13:33:07 +0100

    Add CreateJourneyDTO class for journey creation requests
    Created CreateJourneyDTO.cs in StepManagment.api.DTOS namespace.
    This DTO includes required userId (Guid) and journeyName (string)
    properties to support journey creation functionality.

---
+++ StepManagmentService/src/StepManagment.api/DTOS/CreateJourneyDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace StepManagment.api.DTOS
+{
+    public class CreateJourneyDTO
+    {
+        public required Guid userId { get; set; }
+        public required string journeyName { get; set; }
+
+    }
+}
