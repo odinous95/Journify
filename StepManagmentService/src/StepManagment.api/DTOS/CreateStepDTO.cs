a8dc4ceeb9369ab7d931a255d60e4baaf398d90c
odinous95 <mjd.thif@icloud.com>
2026-01-22 11:33:12 +0100

    Add required JourneyId to CreateStepDTO
    Added a new required property, JourneyId (Guid), to the CreateStepDTO class to ensure each step is associated with a specific journey.

--- StepManagmentService/src/StepManagment.api/DTOS/CreateStepDTO.cs
+++ StepManagmentService/src/StepManagment.api/DTOS/CreateStepDTO.cs
@@ -4,6 +4,8 @@ namespace StepManagment.api.DTOS
 {
     public class CreateStepDTO
     {
+        [Required]
+        public required Guid JourneyId { get; set; }
         [Required, MaxLength(50)]
         public required string Title { get; set; }
         [Required, MaxLength(50)]
