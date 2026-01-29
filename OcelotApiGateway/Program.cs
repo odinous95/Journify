d94a7a376df445f2a6519a8f41a64d1337ac49e9
odinous95 <mjd.thif@icloud.com>
2026-01-29 18:28:37 +0100

    Add JWT auth and CORS to Ocelot API Gateway
    Integrate JWT authentication using ShareLib and configure CORS to allow any origin, header, and method. Update middleware order to ensure authentication and authorization are applied before Ocelot. Add AuthenticationOptions to ocelot.json and reference ShareLib in the project file.

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,10 +1,12 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using OcelotApiGateway.Middleware;
+using ShareLib.SharedExtension;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 builder.Services.AddOcelot(builder.Configuration);
+
 
 builder.Services.AddCors(options =>
 {
@@ -19,20 +21,19 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddControllers();
-
 builder.Logging.AddConsole();
-
+builder.Services.AddJwtAuthentication();
 
 
 var app = builder.Build();
-
-
-
 
 app.MapGet("/", () => "Hello World from Ocelot Api gateway!");
 app.MapControllers();
 app.UseMiddleware<InterceptMiddleware>();
 app.UseCors("AllowAll");
+// Ensure auth order: Authentication then Authorization
+app.UseAuthentication();
+app.UseAuthorization();
 
 await app.UseOcelot();
 
